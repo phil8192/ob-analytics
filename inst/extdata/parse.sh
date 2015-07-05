8687590369cee403e75ca6d9bef3e8e74ad291fc
phil8192 <phil@parasec.net>
2015-07-05 03:30:25 +0300

    limit order events in 1 file

--- inst/extdata/parse.sh
+++ inst/extdata/parse.sh
@@ -14,23 +14,20 @@
 #  1430524797386,0,23102,95228327,1430524797000,65714205,0
 #  1430524799111,0,23037,518983490,1430524780000,65714193,0
 #  1430524799389,0,23036,519006018,1430524799000,65714206,0
-# and then, rows ending with 0 == go into bids.csv, 1 == asks.csv.
 # the format for bids (asks) .csv is:
 #
-# local_timestamp, event_type, price, volume, exchange_timestamp, order_id
+# local_timestamp, event_type, price, volume, exchange_timestamp, order_id, side
 #     where local_timestamp    = time (in milliseconds) when event first received (locally)
 #           event_type         = 0: created, 1: modified, 2: deleted
 #           price              = price level of order event (in lowest denomination) e.g., $321.55 = 32155 cents.
 #           volume             = remaining volume of order (in lowest denomination) e.g., 0.05 Bitcoin = 5000000 Satoshi.
 #           exchange_timestamp = time (in milliseconds) when order first received at exchange.
 #           order_id           = limit order unique identifier.
+#           side               = 0: bid, 1: ask.
 bzcat 2015-05-01.log.bz2 \
   |grep -v order_book \
   |grep -v trade \
   |sed 's/,//g; s/\"//g; s/{//g; s/}//g' \
   |awk '{printf "%d,%d,%d,%d,%d000,%d,%d\n", $1, $2, ($4*10^2), ($6*10^8), $8, $10, $12}' \
   |sed 's/ //g' |sed 's/order_created/0/; s/order_changed/1/; s/order_deleted/2/' >orders.csv
-cat orders.csv |grep ",0$" |sed 's/,0$//' >bids.csv
-cat orders.csv |grep ",1$" |sed 's/,1$//' >asks.csv
-rm -f orders.csv
 
