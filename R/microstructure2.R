9edb5092ff8c34c5f83876a6d4331cb2924ba755
phil8192 <phil@parasec.net>
2015-07-12 23:14:19 +0300

    package + dataset documentation

---
+++ R/microstructure2.R
@@ -0,0 +1,24 @@
+#' microstructure2.
+#'
+#' @name microstructure2
+#' @docType package
+NULL
+
+#' Example pre-processed data.
+#'
+#' 24 hours of Limit Order Book data obtained from the Bitstamp exchange on
+#' 2015-05-01.
+#'
+#' \itemize{
+#'   \item events. Limit order events.
+#'   \item trades. Inferred trades (executions).
+#'   \item depth.  Order book price level depth through time.
+#'   \item depth.summary. Limit order book summary statistics.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name lob.data
+#' @usage data(lob.data)
+#' @format A list containing 4 data frames.
+NULL
